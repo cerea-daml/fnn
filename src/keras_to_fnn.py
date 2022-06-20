f23a5d18e663411f220919cded860f348362e57c
Alban Farchi <alban.farchi@enpc.fr>
2022-06-20 16:25:09 +0200

    added more options to keras_to_fnn

--- src/keras_to_fnn.py
+++ src/keras_to_fnn.py
@@ -50,18 +50,25 @@ def get_model_name(_model, config, **_kwargs):
     return config['name']
 
 
-def get_num_layers(model, _config, **kwargs):
+def get_num_layers(model, config, **kwargs):
     """Returns the total number of layers for a given model."""
-    num_layers = len(model.layers)
+    num_layers = 0
+    for (layer, dropout_rate, subconfig) in zip(model.layers,
+            kwargs['dropout_rates'], config['layers'][1:]):
+        layer_name = get_layer_name(layer, subconfig, **kwargs)
+        if layer_name == 'Dense':
+            num_layers += 1
+            if dropout_rate is not None:
+                num_layers += 1
+        elif layer_name == 'Dropout':
+            if not 'ignore_dropout' in kwargs or not kwargs['ignore_dropout']:
+                num_layers += 1
     key = 'add_norm_in'
     if key in kwargs and kwargs[key]:
         num_layers += 1
     key = 'add_norm_out'
     if key in kwargs and kwargs[key]:
         num_layers += 1
-    for rate in kwargs['dropout_rates']:
-        if rate is not None:
-            num_layers += 1
     return num_layers
 
 
@@ -94,6 +101,9 @@ def get_activation_name(_layer, subconfig, **_kwargs):
     """Returns the activation name for a given layer."""
     return subconfig['config']['activation']
 
+def get_rate(_layer, subconfig, **_kwargs):
+    """Returns the rate for a given layer."""
+    return subconfig['config']['rate']
 
 def add_normalisation_layer(write, input_shape, for_input, **kwargs):
     """Adds content for a normalisation layer."""
@@ -126,6 +136,13 @@ def add_layer(write, input_shape, layer, subconfig, dropout_rate, **kwargs):
             write(INT_FORMAT(output_shape[1]))
             write(FLOAT_FORMAT(dropout_rate))
         return output_shape
+    elif layer_name == 'Dropout':
+        output_shape = layer.compute_output_shape(input_shape)
+        if not 'ignore_dropout' in kwargs or not kwargs['ignore_dropout']:
+            write(STR_FORMAT('dropout'))
+            write(INT_FORMAT(output_shape[1]))
+            write(FLOAT_FORMAT(get_rate(layer, subconfig, **kwargs)))
+        return output_shape
     raise UnsupportedLayerException(layer_name)
 ##\endcond
 
@@ -148,7 +165,8 @@ def keras_to_txt(filename_out, model, **kwargs):
         - Value of `beta` (1d array) for the output normalisation layer, if any.
     - [in] `dropout_rates` : None or float or list of float
         - Whether to add dropout after each internal layer, with the given rate.
-
+    - [in] `ignore_dropout` : bool
+        - Whether to ignore the keras Dropout layers.
     @param[in] filename_out The name of the txt file to write.
     @param[in] model The keras model.
     @param[in] kwargs Key-word arguments.
@@ -181,18 +199,41 @@ def keras_file_to_txt(filename_out, filename_in, **kwargs):
     """! @brief Transforms a keras model file into a txt model file which can be read by FNN.
 
     @details This function is actually a wrapper around \ref keras_to_txt.
-    It accepts the same kwargs.
+    It accepts the same kwargs, with the addition of `custom_objects`.
 
     \b Note
 
     This function uses `tf.keras.models.load_model` to read
     `filename_in`.
+
+    \b Accepted \b kwargs
+    - [in] `custom_objects` : list of str
+        - List of custom objects to ignore while reading keras file.
+    All other kwargs are forwarded to `keras_to_txt`.
     @param[in] filename_out The name of the txt file to write.
     @param[in] filename_in The keras model file.
     @param[in] kwargs Key-word arguments.
+
+    @details \b Accepted \b kwargs
+    - [in] `add_norm_in` : bool
+        - Whether to add a normalisation layer for the input.
+    - [in] `norm_alpha_in` : np.ndarray
+        - Value of `alpha` (1d array) for the input normalisation layer, if any.
+    - [in] `norm_beta_in` : np.ndarray
+        - Value of `beta` (1d array) for the input normalisation layer, if any.
+    - [in] `add_norm_out` : bool
+        - Whether to add a normalisation layer for the output.
+    - [in] `norm_alpha_out` : np.ndarray
+        - Value of `alpha` (1d array) for the output normalisation layer, if any.
     """
     import tensorflow as tf  # pylint: disable=import-outside-toplevel
-    model = tf.keras.models.load_model(filename_in)
+    custom_objects = {
+            key: lambda x,y: 0
+            for key in kwargs['custom_objects']
+            } if 'custom_objects' in kwargs and kwargs['custom_objects'] else {}
+    model = tf.keras.models.load_model(filename_in, custom_objects=custom_objects)
+    if 'custom_objects' in kwargs:
+        del kwargs['custom_objects']
     keras_to_txt(filename_out, model, **kwargs)
 
 
@@ -202,6 +243,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESC)
     parser.add_argument('txt_file', help='output txt file')
     parser.add_argument('keras_file', help='input keras file')
+    parser.add_argument('-c', '--custom', nargs='*', help='list custom keras object to ignore')
+    parser.add_argument('-i', '--ignore', action='store_true', help='whether to ignore dropout layers')
     args = parser.parse_args()
-    keras_file_to_txt(args.txt_file, args.keras_file)
+    keras_file_to_txt(args.txt_file, args.keras_file, custom_objects=args.custom, ignore_dropout=args.ignore)
 ##\endcond
